824a65b3df2cde3b70dcc249f74beda9307056bf
Rob Cannon <rob.cannon@equifax.com>
2020-10-13 16:52:00 -0400

    Still fixing identity

--- .github/scripts/update-tags.ps1
+++ .github/scripts/update-tags.ps1
@@ -3,6 +3,9 @@ param (
   $MajorVersion = '1'
 )
 
+git config user.email "github@cannonsoftware.com"
+git config user.name "GitHub Action"
+
 $versions = $(git tag) | ?{ $_ -match "^v$MajorVersion\.\d+" } | %{ [Version]($_ -replace 'v','') }
 $max_version = $versions | Measure-Object -Maximum | % Maximum
 $next_version = $max_version ? [Version]::new($max_version.Major, $max_version.Minor + 1) : [Version]::new($MajorVersion,0)
@@ -13,6 +16,6 @@ Write-Host "Setting tag for $tag"
 # Add the new tag
 Write-Host "Adding $tag"
 git tag -a $tag -m $tag -f
-git -c user.name='GitHubAction' -c user.email='my@email.org' push origin $tag
+git push origin $tag
 
 Write-Output "::set-output name=tag=::$tag"
